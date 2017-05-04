81aaddfbb6ccac8a9e5a13156a589117d12b99ce
Angelo Medeiros <angeloprogrammer@gmail.com>
2017-05-04 13:53:15 -0300

    TRABALHANDO COM VARIAVEIS -Testando a ordem -Comandos, console.log com typeof

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log('Hello world from external scripts')
+console.log('Hello world from external scripts');
+
+var numero  = 3;
+var numero1 = 1;
+
+console.log(typeof numero);
+
+numero = "Angelo " + numero1;
+
+console.log(numero);
+
+numero = numero + " " + numero1;
+
+console.log(numero);
+
+numero = 6;
+
+numero = 6 + numero1;
+
+console.log(typeof numero);
+console.log(numero);
+
+function myFunction(){
+	var numero = 0.1+123;
+	console.log(numero);
+}
+
+myFunction();
+
+// Se tirar o my, a função será do tipo anônima
+var myfunction = function my(){      
+	var numero = 0.1+123;
+	console.log(numero);
+}
+
+console.log(typeof myfunction);
+myfunction();
